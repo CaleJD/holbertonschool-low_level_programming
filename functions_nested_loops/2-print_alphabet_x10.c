47a99fefb648c43cb2936cd41a3eba8800d80a21
Cale <14838@holbertonstudents.com>
2026-07-09 08:55:13 +0000

    fixed 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,21 +12,17 @@ void print_alphabet_x10(void)
 	char letter;
 	int count;
 
-	letter = 'a';
 	count = 0;
 	while (count <= 9)
 	{
 		count = count + 1;
-		while (letter < 'z')
+		letter = 'a';
+		while (letter <= 'z')
 		{
 			_putchar(letter);
-			letter = letter + 1;
+			letter++;
+			
 		}
-		while (letter == 'z')
-		{
-			_putchar(letter);
-			letter = 'a';
-		}
+		_putchar('\n')
 	}
-	_putchar('\n');
 }
