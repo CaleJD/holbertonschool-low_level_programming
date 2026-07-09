3c9ad3ad93934c0ee5903bf90fd2589e5a1ae4e6
Cale <14838@holbertonstudents.com>
2026-07-09 06:53:03 +0000

    added file 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Counts from 0-9
+ * Description: prints a number and
+ * add +1
+ * Return: 0
+ */
+
+int main(void)
+{
+	char num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != 9)
+		{
+			putchar(',');
+		}
+		num = num + 1;
+	}
+	putchar('\n');
+	return (0);
+}
