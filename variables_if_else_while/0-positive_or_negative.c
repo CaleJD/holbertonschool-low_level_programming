893694ee0980456a775a6780e6571ea51cf482c8
Cale <14838@holbertonstudents.com>
2026-07-09 02:28:49 +0000

    add 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - RNG calculator
+* Description: tells if rng is higher or lower or 0
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("negative\n");
+	else if (n == 0)
+		printf("is zero\n");
+	else
+		printf("positive\n");
+	return (0);
+}
