f1397aa5d1931508a50773df0ed5fb3e77a1cc7f
Cale <14838@holbertonstudents.com>
2026-07-09 05:13:12 +0000

    added spaces

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d", n);
 	if (n % 10 > 5)
-		printf("and is greater than 5");
+		printf(" and is greater than 5");
 	if (n % 10 == 0)
-		printf("and is 0");
+		printf(" and is 0");
 	if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0");
 	return (0);
 }
