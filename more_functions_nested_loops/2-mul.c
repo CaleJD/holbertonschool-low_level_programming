8487c2aa66c7fc4cc680639ec1375079423cdd7f
Cale <14838@holbertonstudents.com>
2026-07-10 05:33:53 +0000

    add 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - multiplies 2 integers together to give a sum
+ * @a: digit 1
+ * @b: digit 2
+ * Return: returns the sum of the process
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = (a * b);
+	return (sum);
+}
