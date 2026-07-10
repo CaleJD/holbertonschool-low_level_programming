7d348746bf1d30f0a31993f3c2149e0034f370ee
Cale <14838@holbertonstudents.com>
2026-07-10 01:13:07 +0000

    add file 6 and update main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Gets the absolute value
+ * of an integer
+ * @n: The integer to make absolute
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
