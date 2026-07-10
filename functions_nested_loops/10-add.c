b77127141c35d2b48377adcadb61e3feba7fae50
Cale <14838@holbertonstudents.com>
2026-07-10 03:38:17 +0000

    add 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers together to give a sum
+ * @a: digit 1
+ * @b: digit 2
+ * Return: returns the sum of the process
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+	return (sum);
+}
