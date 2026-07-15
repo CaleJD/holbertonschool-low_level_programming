46f8e56f6bcb958de5c52ad8d685ad2b0908bd47
Cale <14838@holbertonstudents.com>
2026-07-15 18:00:36 +1000

    add 2 and update main

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - Gets the length of a variable and
+* returns it
+* @length: The amount of characters in a variable
+* Return: the length
+*/
+int _strlen(char *s)
+{
+	int length;
+	
+	length = strlen(s);
+	return (length);
+}
