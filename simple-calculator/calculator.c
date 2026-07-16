40fcbe1bdd273c071262478a4c77da30156151b3
Cale <14838@holbertonstudents.com>
2026-07-16 13:31:02 +1000

    added an invalid choice reply

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -6,20 +6,24 @@
 */
 int main(void)
 {
-    int choice;
+	int choice;
 
-    printf("Simple Calculator\n");
-    printf("1) Add\n");
-    printf("2) Subtract\n");
-    printf("3) Multiply\n");
-    printf("4) Divide\n");
-    printf("0) Quit\n");
-    printf("Choice: ");
+	printf("Simple Calculator\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	printf("Choice: ");
 
-    scanf("%d", &choice);
-
-    if (choice == 0)
-        printf("Bye!\n");
-
-    return (0);
+	scanf("%d", &choice);
+	if (choice < 0 || choice > 4)
+	{
+		printf("Invalid choice\n");
+	}
+	if (choice == 0)
+	{
+		printf("Bye!\n");
+	}
+	return (0);
 }
