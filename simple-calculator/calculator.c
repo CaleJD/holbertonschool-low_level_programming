7b98ebc14f2c57731c391a562921a54a906bdf96
Cale <14838@holbertonstudents.com>
2026-07-16 14:29:31 +1000

    added addition to the calc

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
 /**
-* main - A simple Calculator
-* 
+* addition - Adds 2 numbers together
+* @a: First number
+* @b: Second number
+* Return: sum of a and b
 */
+int addition(int a, int b)
+{
+	int result;
+
+	result = (a + b);
+	return (result);
+}
+
+/**
+* main - A simple Calculator
+* @a: First number
+* @b: Second number
+* Return: 0
+*/
+int addition(int a, int b);
 int main(void)
 {
 	int choice;
+	int a;
+	int b;
+	int result;
 
 	printf("Simple Calculator\n");
 	printf("1) Add\n");
@@ -15,11 +35,19 @@ int main(void)
 	printf("4) Divide\n");
 	printf("0) Quit\n");
 	printf("Choice: ");
-
 	scanf("%d", &choice);
 	if (choice < 0 || choice > 4)
 	{
 		printf("Invalid choice\n");
+	}
+	if (choice == 1)
+	{
+		printf("First number: ");
+		scanf("%d", &a);
+		printf("Second number: ");
+		scanf("%d", &b);
+		result = addition(a, b);
+		printf("Result: %d\n", result);
 	}
 	if (choice == 0)
 	{
