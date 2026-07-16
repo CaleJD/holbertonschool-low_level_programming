0d7f37fdd54c34c981cab605806618eec65d2aca
Cale <14838@holbertonstudents.com>
2026-07-16 12:00:47 +1000

    add 6 and update main

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _puts - prints out every 2nd letter of a string
+* @str: string to print
+*/
+void _puts2(char *str)
+{
+        int currentLet;
+
+        currentLet = 0;
+        while (str[currentLet] != '\0')
+        {
+                _putchar(str[currentLet]);
+                currentLet++;
+		currentLet++;
+        }
+        _putchar('\n');
+}
