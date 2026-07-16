ce201de20b80222a3c4602dfb937eac3d367e0fb
Cale <14838@holbertonstudents.com>
2026-07-16 11:33:15 +1000

    fixed 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
-* print_rev - prints a string in reverse
-* @s: string to print
+* print_rev - Prints a string in reverse.
+* @s: String to print.
 */
 void print_rev(char *s)
 {
 	int currentLet;
 
-	currentLet = '\0';
-	while (s[currentLet] >= 0)
+	currentLet = 0;
+	while (s[currentLet] != '\0')
+	{
+		currentLet++;
+	}
+	currentLet--;
+	while (currentLet >= 0)
 	{
 		_putchar(s[currentLet]);
 		currentLet--;
