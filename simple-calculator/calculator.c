f5e487f4f3b0ed14394a4538bb0606fe17343377
Cale <14838@holbertonstudents.com>
2026-07-16 12:17:29 +1000

    Task 0 for calc

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - A simple Calculator
+* 
+*/
+int main(void)
+{
+    int choice;
+
+    printf("Simple Calculator\n");
+    printf("1) Add\n");
+    printf("2) Subtract\n");
+    printf("3) Multiply\n");
+    printf("4) Divide\n");
+    printf("0) Quit\n");
+    printf("Choice: ");
+
+    scanf("%d", &choice);
+
+    if (choice == 0)
+        printf("Bye!\n");
+
+    return (0);
+}
