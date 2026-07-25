af74ed4b784dd7d6dd435950899a16e7bed46a98
Cale <14838@holbertonstudents.com>
2026-07-25 16:12:28 +1000

    add 2 and update dog.h

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* print_dog - Prints out the info for the dog struct
+* @d: The pointer to the dog to print
+*/
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", my_dog.name);
+	printf("Age: %.1f\n", my_dog.age);
+	printf("Owner: %s\n", my_dog.owner);
+}
