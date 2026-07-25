dc38c5f1812446cbe07c663f6a9bd968b0fcb8ab
Cale <14838@holbertonstudents.com>
2026-07-25 15:19:37 +1000

    added dog struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
-* dog - The struct containing the information for a dog
+* struct dog - The struct containing the information for a dog
+* @name: Name of dog
+* @age: Age of dog
+* @owner: Owner of dog
+* Description: Header file that defines a struct of a dog
 */
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
