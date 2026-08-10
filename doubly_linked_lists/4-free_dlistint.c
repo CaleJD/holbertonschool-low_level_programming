24d8240ce107c35de77571551f6593b0a0ebd69f
Cale <14838@holbertonstudents.com>
2026-08-10 15:53:32 +1000

    added 4 and updated lists.h

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: pointer to the head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->next;
+		free(head);
+		head = next_node;
+	}
+}
