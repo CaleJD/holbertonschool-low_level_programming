c3032ccec15e13b3372342cea6b59eea08ac7d08
Cale <14838@holbertonstudents.com>
2026-08-10 15:14:37 +1000

    made betty compliant and change stdio to stdlib

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,24 +1,29 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * add_dnodeint - Adds a new node to the head
- * @head: pointer to the head of the node
- * @n: The data in the new node
+ * add_dnodeint - Adds a new node at the beginning of a list
+ * @head: Pointer to the head of the list
+ * @n: Data to store in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-    new_node->n = n;
-    new_node->next = *head;
-    new_node->prev = NULL;
-    if (*head != NULL)
-        (*head)->prev = new_node;
-    *head = new_node;
 
-    return (new_node);
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
 }
