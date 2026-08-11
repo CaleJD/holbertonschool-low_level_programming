39fb20db2205b8dfda8d3a38696376a314c19ca5
Cale <14838@holbertonstudents.com>
2026-08-11 13:14:18 +1000

    added 8 and updated main

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,26 +17,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	current = *h;
 	check = 0;
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	while (current != NULL && check < idx)
 	{
 		current = current->next;
 		check++;
 	}
-
 	if (current == NULL && check < idx)
 		return (NULL);
-
 	if (current == NULL && check == idx)
 		return (add_dnodeint_end(h, n));
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->next = current;
 	new_node->prev = current->prev;
---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at a given index
+ * @head: pointer to the head of the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 on success, or -1 if the index is invalid
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int check;
+	dlistint_t *current;
+
+	if (*head == NULL)
+		return (-1);
+	current = *head;
+	check = 0;
+	if (index == 0)
+	{
+		*head = current->next;
+
+		if (*head != NULL)
+			(*head)->prev = NULL;
+
+		free(current);
+		return (1);
+	}
+	while (current->next != NULL && check < index)
+	{
+		current = current->next;
+		check++;
+	}
+	if (current->next == NULL && check < index)
+		return (-1);
+
+	if (current->next == NULL && check == index)
+	{
+		current->prev->next = NULL;
+		free(current);
+		return (1);
+	}
+	if (check == index)
+	{
+		current->prev->next = current->next;
+		current->next->prev = current->prev;
+		free(current);
+		return (1);
+	}
+	return (-1);
+}
