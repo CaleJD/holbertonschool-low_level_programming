1ab7134b9ca68e26fba3e7bcbdcd9fb5920577b4
Cale <14838@holbertonstudents.com>
2026-08-16 22:05:29 +1000

    added file 3 and updated header

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -14,8 +14,22 @@ typedef struct hash_table
 	void **array;
 } hash_table_t;
 
+/**
+ * struct hash_node - Hash table node
+ * @key: Key
+ * @value: Value associated with the key
+ * @next: Pointer to the next node
+ */
+typedef struct hash_node
+{
+	char *key;
+	char *value;
+	struct hash_node *next;
+} hash_node_t;
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif
