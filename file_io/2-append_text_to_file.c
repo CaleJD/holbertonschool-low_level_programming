74f8a93a6fb1d5752f14299ceccb5f54cd7b66b2
Cale <14838@holbertonstudents.com>
2026-08-21 22:45:25 +1000

    added file 2 and updated header

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text to the end of a file
+ * @filename: The name of the file
+ * @text_content: The text to append
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int descriptor;
+	size_t length;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		length = 0;
+		while (text_content[length] != '\0')
+			length++;
+
+		bytes_written = write(descriptor, text_content, length);
+		if (bytes_written != (ssize_t)length)
+		{
+			close(descriptor);
+			return (-1);
+		}
+	}
+
+	if (close(descriptor) == -1)
+		return (-1);
+
+	return (1);
+}
