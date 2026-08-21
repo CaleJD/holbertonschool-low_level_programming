743e8b1e9ecc4260e576a69f2956d473ce8371df
Cale <14838@holbertonstudents.com>
2026-08-21 16:37:15 +1000

    added file 6 and updated header

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * check_prime - Checks whether n has a divisor
+ * @n: Number being checked
+ * @divisor: Current divisor
+ *
+ * Return: 1 if prime, otherwise 0
+ */
+int check_prime(int n, int division)
+{
+	if (division == n)
+		return (1);
+
+	if (n % division == 0)
+		return (0);
+
+	return (check_prime(n, division + 1));
+}
+
+/**
+ * is_prime_number - Determines whether n is prime
+ * @n: Number being checked
+ *
+ * Return: 1 if n is prime, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (check_prime(n, 2));
+}
