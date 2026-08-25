3c30d4fd378284d5d29f0269b093ec005ade8977
Cale <14838@holbertonstudents.com>
2026-08-25 14:51:06 +1000

    addef fiile 8 and updated header

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - print a certain number of ints in an array
+ * @a: array of integers
+ * @n: number of integers to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", (a[i]));
+		if (i != n - 1)
+			printf(",");
+		i++;
+	}
+	printf("\n");
+}
