0a6966dc8e183e2a7bf7305ea04cfaeef198a222
Cale <14838@holbertonstudents.com>
2026-08-25 13:48:59 +1000

    added file 7 and updated header

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* puts_half - prints out half a string
+* @str: string to print
+*/
+void puts_half(char *str)
+{
+	int currentLet;
+	int length;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	length = length / 2;
+	currentLet = 0;
+	while (currentLet < length)
+	{
+		_putchar(str[currentLet]);
+		currentLet++;
+	}
+	_putchar('\n');
+}
