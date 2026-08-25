b4023d1888d8f16b07c043e93aa94e4c18485e6c
Cale <14838@holbertonstudents.com>
2026-08-25 15:24:48 +1000

    added file 9 and updated header

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcopy - Copies a string to a destination
+ * @dest: Destination of copied string
+ * @src: Source of string to copy
+ * Return: Pointer to the copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	
+	return (dest);
+}
