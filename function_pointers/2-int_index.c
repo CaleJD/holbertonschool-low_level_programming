648257013e81194f804f87197301d7303b91fadd
Cale <14838@holbertonstudents.com>
2026-09-07 14:25:44 +1000

    added file 2 and updated header

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - compares an int in an array
+ * @array: array with ints to compare
+ * @size: size of array
+ * @cmp: pointer to func to compare ints
+ * Return: 1 if func found matching int, 0 if not
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		if ((cmp(array[i] != 1)))
+			i++;
+
+		if ((cmp(array[i] == 1)))
+			return (1);
+	}
+	return (0);
+}
