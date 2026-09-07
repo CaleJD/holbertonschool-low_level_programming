c48bc5ff1d34a3ac7f70d6fe8a6f4050df12c68b
Cale <14838@holbertonstudents.com>
2026-09-07 14:42:06 +1000

    fix?

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	while (i < size)
 	{
-		if ((cmp(array[i] != 1 && i <= size)))
+		if (cmp(array[i]) != 1 && i <= size)
 			i++;
 
 		if ((cmp(array[i] == 1)))
