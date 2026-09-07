68b8fb8eb34997a4fe7709e8aa14b3b00b74ebbd
Cale <14838@holbertonstudents.com>
2026-09-07 14:00:59 +1000

    added file 1 and updated header

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through an array and completes an action of each
+ * @array: array to iterate
+ * @size: size of the array
+ * @action: pointer to func of action to perform
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (i <= size)
+	{
+		action(array[i]);
+	}
+}
