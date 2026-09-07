89ec92dadee4ebf52057cab2da96ba01a1ffbcb7
Cale <14838@holbertonstudents.com>
2026-09-07 14:45:10 +1000

    final fixes

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -5,18 +5,21 @@
  * @array: array with ints to compare
  * @size: size of array
  * @cmp: pointer to func to compare ints
- * Return: 1 if func found matching int, 0 if not
+ * Return: 1 if func found matching int, -1 if not
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 
 	while (i < size)
 	{
 		if (cmp(array[i]) != 1 && i <= size)
 			i++;
 
-		if ((cmp(array[i] == 1)))
+		if ((cmp(array[i]) == 1))
 			return (i);
 	}
 	return (-1);
