7ad1749f43ed836fed8d7a3c3467b177b53f2114
Cale <14838@holbertonstudents.com>
2026-09-14 21:35:56 +1000

    too far gone go bacl!

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -14,10 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	i = 0;
 	while (i < n)
 	{
-		if (va_arg(strs, char *) == NULL)
-			printf("(nil)");
-		if (va_arg(strs, char *) != NULL)
-			printf("%s", va_arg(strs, char *));
+		printf("%s", va_arg(strs, char *));
 		if ((i + 1) != n)
 		{
 			if (separator != NULL)
