1092299d9a803bea9861037c3c2977303e541511
Cale <14838@holbertonstudents.com>
2026-09-14 20:03:13 +1000

    start of var func learning project

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - collects integers and sums them
+ * @n: number of ints to receive
+ * Return: total of ints
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	int i;
+	int total;
+
+	i = 0;
+	total = 0;
+	if (n == 0)
+		return (0);
+	va_start(nums, n);
+	while (i < n)
+	{
+		total += va_arg(nums, int);
+		i++;
+	}
+	return (total);
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is my holberton practice for variadic functions
