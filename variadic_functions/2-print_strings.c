a10efe171d6c15a02aeaafa0e93a12be34385d87
Cale <14838@holbertonstudents.com>
2026-09-14 21:42:19 +1000

    oopsy daisy it was backwords

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,9 +17,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(strs, char *);
 		if (string == NULL)
-			printf("%s", string);
-		if (string != NULL)
 			printf("(nil)");
+		if (string != NULL)
+			printf("%s", string);
 		if ((i + 1) != n)
 		{
 			if (separator != NULL)
