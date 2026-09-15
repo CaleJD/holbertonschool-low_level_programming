1eab9a24cc4d9aaaa4dafb772999fbb4deb400e7
Cale <14838@holbertonstudents.com>
2026-09-15 14:37:26 +1000

    added file 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of args
+ * @argc: number of args
+ * @argv: list of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void) argv;
+	return (0);
+}
