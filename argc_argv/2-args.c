a86d7c79c8ab6334c594861728582f55259744d0
Cale <14838@holbertonstudents.com>
2026-09-15 14:42:22 +1000

    added file 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all args given
+ * @argc: number of args
+ * @argv: list of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
