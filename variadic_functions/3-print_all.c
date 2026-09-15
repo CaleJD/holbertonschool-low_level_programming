8d29ac52c00c7baa7e4a50cc903c0bd19d2cdf66
Cale <14838@holbertonstudents.com>
2026-09-15 14:05:40 +1000

    added a printed checker

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * print_all - prints out all args
@@ -9,7 +10,9 @@ void print_all(const char * const format, ...)
 	va_list strs;
 	unsigned int i;
 	char *string;
+	int printed;
 
+	printed = 0;
 	va_start(strs, format);
 	i = 0;
 	if (format == NULL)
@@ -20,21 +23,34 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(strs, int));
+				printed = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(strs, int));
+				printed = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(strs, double));
+				printed = 1;
 				break;
 			case 's':
 				string = va_arg(strs, char *);
 				printf("%s", string == NULL ? "(nil)" : string);
+				printed = 1;
 				break;
 		}
-		if (format[i + 1] != '\0')
+		if (format[i + 1] != '\0' && printed == 1)
+		{
 			printf(", ");
+			printed = 0;
+		}
 		i++;
 	}
 	printf("\n");
+	va_end(strs);
+}
+
+void main(void)
+{
+	print_all("mnbvfqcepolsbxzi", 3.14435, 'H', "#Cisfun", 0);
 }
