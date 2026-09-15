897fa906bf6f863f3ae84401200042c9c845a46e
Cale <14838@holbertonstudents.com>
2026-09-15 14:33:13 +1000

    betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,10 +4,11 @@
  * main - prints the name of the file
  * @argc: number of args
  * @argv: list of args
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
