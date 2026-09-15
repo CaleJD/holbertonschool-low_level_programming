8906cd116ab30e598e780502b73aa3386efba771
Cale <14838@holbertonstudents.com>
2026-09-15 14:26:39 +1000

    added file 0 and readme

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: number of args
+ * @argv: list of args
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return 0;
+}
